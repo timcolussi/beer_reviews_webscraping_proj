d2a7c40fa0a26ee83dbbdc4f9695174807d6c6fa
Tim Colussi <tim.colussi@gmail.com>
2020-06-30 23:45:45 -0400

    more work on selenium file

--- beer_reviews_selenium.py
+++ beer_reviews_selenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import re
-
+import time
+import csv
 
 # set driver
 driver = webdriver.Chrome(r'\Users\timco\Downloads\chromedriver_win32\chromedriver.exe')
@@ -9,56 +10,79 @@ driver = webdriver.Chrome(r'\Users\timco\Downloads\chromedriver_win32\chromedriv
 # navigate to page
 driver.get("https://beerandbrewing.com/beer-reviews/?q=&hPP=30&idx=cbb_web_review_search&p=0")
 
-links = driver.find_elements_by_xpath('//div[@class="hit-content col-xs-12 col-sm-12 col-md-10 text-wrapper"]/a')
+index = 1
 hrefs = []
-for li in links:
-	hrefs.append(li.get_attribute('href'))
+while index <=2:
+	print("Scraping Page number " + str(index))
+	index = index + 1
+	links = driver.find_elements_by_xpath('//div[@class="hit-content col-xs-12 col-sm-12 col-md-10 text-wrapper"]/a')
+	for li in links:
+		hrefs.append(li.get_attribute('href'))
+	# insert a scroll
+	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+	# locate and click the next button
+	next_button = driver.find_element_by_xpath('//*[@id="pagination"]/div/ul/li[10]/a')
+	next_button.click()
+	time.sleep(2)
 
-mouthfeel_scores = []
+csv_file = open('test.csv', 'w', encoding='utf-8', newline='')
+writer = csv.writer(csv_file)
 for href in hrefs:
+	beers_dict = {}
 	driver.get(href)
-	#name = driver.find_element_by_xpath('//*[@id="article-body"]/h1').text
-	#style = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[2]/p[1]').text
-	# try:
-	# 	b_comment = driver.find_element_by_xpath('//*[@id="article-body"]/div[3]/p[1]').text
-	# 	brewer_comments.append(b_comment)
-	# except:
-	# 	b_comment = driver.find_element_by_xpath('//*[@id="article-body"]/div[2]/p[1]').text
-	# 	brewer_comments.append(b_comment)
-	# try:
-	# 	p_comment = driver.find_element_by_xpath('//*[@id="article-body"]/div[3]/p[2]').text
-	# 	panel_comments.append(p_comment)
-	# except:
-	# 	p_comment = driver.find_element_by_xpath('//*[@id="article-body"]/div[2]/p[2]').text
-	# 	panel_comments.append(p_comment)
-	# try:
-	# 	e_comment = driver.find_element_by_xpath('//*[@id="article-body"]/div[3]/p[3]').text
-	# 	editor_comments.append(e_comment)
-	# except:
-	# 	e_comment = driver.find_element_by_xpath('//*[@id="article-body"]/div[2]/p[3]').text
-	# 	editor_comments.append(e_comment)
-	# abv = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[2]/p[2]').text
-	# abv = re.findall('\d*\.?\d+', abv)[0]
-	# try:
-	# 	ibu = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[2]/p[2]').text
-	# 	ibu = re.findall('\d*\.?\d+', ibu)[1]
-	# 	IBUs.append(ibu)
-	# except:
-	# 	pass
-	# score = driver.find_element_by_xpath('//div[@class="main-score-overall rating col-12"]').text
-	# score = re.findall('\d+', score)[0]
-	#aroma = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[1]/td[2]').text
-	#appearance = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[2]/td[2]').text
-	#flavor = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[3]/td[2]').text
+	name = driver.find_element_by_xpath('//*[@id="article-body"]/h1').text
+	style = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[2]/p[1]').text
+	try:
+	 	brewer_comments = driver.find_element_by_xpath('//*[@id="article-body"]/div[3]/p[1]').text
+	except:
+	 	brewer_comments = driver.find_element_by_xpath('//*[@id="article-body"]/div[2]/p[1]').text	 	
+	try:
+	 	panel_comments = driver.find_element_by_xpath('//*[@id="article-body"]/div[3]/p[2]').text	 
+	except:
+	 	panel_comments = driver.find_element_by_xpath('//*[@id="article-body"]/div[2]/p[2]').text	 	
+	try:
+	 	editor_comments = driver.find_element_by_xpath('//*[@id="article-body"]/div[3]/p[3]').text	 	
+	except:
+	 	editor_comments = driver.find_element_by_xpath('//*[@id="article-body"]/div[2]/p[3]').text	 	
+	try:
+		ABV = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[2]/p[2]').text
+		ABV = re.findall('\d*\.?\d+', ABV)[0]
+	except:
+		pass
+	try:
+	 	IBU = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div[2]/p[2]').text
+	 	IBU = re.findall('\d*\.?\d+', IBU)[1]	 	
+	except:
+	 	pass
+	score = driver.find_element_by_xpath('//div[@class="main-score-overall rating col-12"]').text
+	score = re.findall('\d+', score)[0]
+	aroma = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[1]/td[2]').text
+	appearance = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[2]/td[2]').text
+	flavor = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[3]/td[2]').text
 	mouthfeel = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/table/tbody/tr[4]/td[2]').text
-	mouthfeel_scores.append(mouthfeel)
 
+	beers_dict['name'] = name
+	beers_dict['style'] = style
+	beers_dict['brewer_comments'] = brewer_comments
+	beers_dict['panel_comments'] = panel_comments
+	beers_dict['editor_comments'] = editor_comments
+	beers_dict['ABV'] = ABV
+	beers_dict['IBU'] = IBU
+	beers_dict['score'] = score
+	beers_dict['aroma']	= aroma
+	beers_dict['appearance'] = appearance
+	beers_dict['flavor'] = flavor
+	beers_dict['mouthfeel'] = mouthfeel
+
+	writer.writerow(beers_dict.values())	
 
 
 #print(hrefs)
 #print(len(hrefs))
-print(mouthfeel_scores)
-print(len(mouthfeel_scores))
+# print(hrefs)
+# print(len(hrefs))
+# print(names)
+# print(len(names))
 
 
 driver.close()
