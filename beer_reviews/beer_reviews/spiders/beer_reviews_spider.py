499e34709aef8718a879268721a21e3a97fd14f7
Tim Colussi <tim.colussi@gmail.com>
2020-07-02 07:01:08 -0400

    Analysis in jupyter notebook

--- beer_reviews/beer_reviews/spiders/beer_reviews_spider.py
+++ beer_reviews/beer_reviews/spiders/beer_reviews_spider.py
@@ -7,4 +7,10 @@ class BeerReviewsSpider(Spider):
 	start_urls = ['https://beerandbrewing.com/beer-reviews/?q=&hPP=30&idx=cbb_web_review_search&p=0']
 
 	def parse(self, response):
+		result_urls = [f'https://beerandbrewing.com/beer-reviews/?q=&hPP=30&idx=cbb_web_review_search&p={i}' for i in range(34)]
+
+		for url in result_urls:
+			yield Request(url=url, callback=self.parse_results_page)
+
+	def parse_results_page(self, response):
 		pass
